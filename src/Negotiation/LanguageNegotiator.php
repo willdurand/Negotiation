34fcc4c05c4c323d03cd2a02bc3dbddba12f41dc
cblegare <cblegare.atl@ntis.ca>
2014-01-16 14:18:50 -0500

    Issue17: Allow generic language representation to be mapped by specific ones

--- src/Negotiation/LanguageNegotiator.php
+++ src/Negotiation/LanguageNegotiator.php
@@ -44,4 +44,29 @@ class LanguageNegotiator extends Negotiator
 
         return $this->sortAcceptHeaders($acceptHeaders, $catchAll);
     }
+
+    /**
+     * {@inheritDoc}
+     *
+     * This implementation provide matching between generic Accept-Language and localized
+     * priorities.
+     * For instance, 'en' can be matched to 'en-US'.
+     */
+    protected function matchHeaderInPriorities($accept, $priorities)
+    {
+        $needle = strtolower($accept->getValue());
+
+        $trimedPriorities = array_map(
+            function($value){
+                return strtok($value, '-');
+            },
+            $priorities
+        );
+
+        if (in_array($needle, $priorities) || in_array($needle, $trimedPriorities)) {
+            return true;
+        }
+
+        return false;
+    }
 }
