1ce6edf3e380c4404de36ee6621e8d2552d4baff
neural-wetware <neural-wetware@mailinator.com>
2015-05-17 22:18:37 +1000

    using new AcceptHeader constructor

--- tests/Negotiation/Tests/AcceptHeaderTest.php
+++ tests/Negotiation/Tests/AcceptHeaderTest.php
@@ -7,9 +7,7 @@ use Negotiation\Negotiator;
 
 class AcceptHeaderTest extends TestCase
 {
-
-    protected function call_private_method($class, $method, $object, $params)
-    {
+    protected function call_private_method($class, $method, $object, $params) {
         $method = new \ReflectionMethod($class, $method);
 
         $method->setAccessible(TRUE);
@@ -17,26 +15,16 @@ class AcceptHeaderTest extends TestCase
         return $method->invokeArgs($object, $params);
     }
 
-    /**
-     * @var AcceptHeader
-     */
-    private $acceptHeader;
-
-    protected function setUp()
-    {
-        $this->acceptHeader = new AcceptHeader('foo', 1.0, array(
-            'hello' => 'world',
-        ));
-    }
-
     public function testGetParameter()
     {
-        $this->assertTrue($this->acceptHeader->hasParameter('hello'));
-        $this->assertEquals('world', $this->acceptHeader->getParameter('hello'));
+        $acceptHeader = new AcceptHeader('foo; q=1; 1.0 ; hello=world');
 
-        $this->assertFalse($this->acceptHeader->hasParameter('unknown'));
-        $this->assertNull($this->acceptHeader->getParameter('unknown'));
-        $this->assertFalse($this->acceptHeader->getParameter('unknown', false));
+        $this->assertTrue($acceptHeader->hasParameter('hello'));
+        $this->assertEquals('world', $acceptHeader->getParameter('hello'));
+
+        $this->assertFalse($acceptHeader->hasParameter('unknown'));
+        $this->assertNull($acceptHeader->getParameter('unknown'));
+        $this->assertFalse($acceptHeader->getParameter('unknown', false));
     }
 
     /**
@@ -44,7 +32,7 @@ class AcceptHeaderTest extends TestCase
      */
     public function testIsMediaRange($value, $expected)
     {
-        $header = new AcceptHeader($value, 1.0);
+        $header = new AcceptHeader($value);
 
         $this->assertEquals($expected, $header->isMediaRange());
     }
@@ -59,44 +47,74 @@ class AcceptHeaderTest extends TestCase
     }
 
     /**
-     * @dataProvider dataProviderForTestGetMediaType
+     * @dataProvider dataProviderForParseParameters
      */
-    public function testGetMediaType($acceptHeader, $expectedType)
+    public function testParseParameters($value, $expected)
     {
-        $negotiator = new Negotiator();
-        $parameters = $this->call_private_method('\Negotiation\Negotiator', 'parseParameters', $negotiator, array($acceptHeader));
+        $acceptHeader = new AcceptHeader($value);
+        list($media_type, $parameters) = $this->call_private_method('\Negotiation\AcceptHeader', 'parseParameters', null, array($value));
 
-        $acceptHeader = new AcceptHeader($acceptHeader, 1.0, $parameters);
-        $mt = $acceptHeader->getMediaType();
+        $this->assertCount(count($expected), $parameters);
 
-        $this->assertEquals($expectedType, $mt);
-
+        foreach ($expected as $key => $value) {
+            $this->assertArrayHasKey($key, $parameters);
+            $this->assertEquals($value, $parameters[$key]);
+        }
     }
 
-    public static function dataProviderForTestGetMediaType()
+    public static function dataProviderForParseParameters()
     {
         return array(
-            array('text/html;hello=world', 'text/html'), # with param
-            array('application/pdf', 'application/pdf'), # without param
-            array('application/xhtml+xml;q=0.9', 'application/xhtml+xml'),
-            array('text/plain; q=0.5', 'text/plain'),
-            array('text/html;level=2;q=0.4', 'text/html'),
-            array('text/html ; level = 2   ; q = 0.4', 'text/html'),
-            array('text/*', 'text/*'),
-            array('text/* ;q=1 ;level=2', 'text/*'),
-            array('*/*', '*/*'),
-            array('*/* ; param=555', '*/*'),
-            array('TEXT/hTmL;leVel=2; Q=0.4', 'TEXT/hTmL'),
+            array(
+                'application/json ;q=1.0; level=2;foo= bar',
+                array(
+                    'q' => 1.0,
+                    'level' => 2,
+                    'foo'   => 'bar',
+                ),
+            ),
+            array(
+                'application/json ;q = 1.0; level = 2;     FOO  = bAr',
+                array(
+                    'q' => 1.0,
+                    'level' => 2,
+                    'foo'   => 'bAr',
+                ),
+            ),
+            array(
+                'application/json;q=1.0',
+                array(
+                    'q' => 1.0,
+                ),
+            ),
+            array(
+                'application/json;foo',
+                array(),
+            ),
+        );
+    }
 
-            # language
-            array('da', 'da'),
-            array('en-gb;q=0.8', 'en-gb'),
-            array('en-GB;q=0.8', 'en-GB'),
-            array('es;q=0.7', 'es'),
-            array('fr ; q= 0.1', 'fr'),
+    /**
+     * @dataProvider dataProviderBuildParametersString
+     */
 
-            array('', null),
-            array(null, null),
+    public function testBuildParametersString($value, $expected) {
+        $string = $this->call_private_method('\Negotiation\AcceptHeader', 'buildParametersString', null, array($value));
+
+        $this->assertEquals($string, $expected);
+    }
+
+    public static function dataProviderBuildParametersString()
+    {
+        return array(
+            array(
+                array(
+                    'q' => '1.0',
+                    'level' => '2',
+                    'foo'   => 'bar',
+                ),
+                'q=1.0;level=2;foo=bar',
+            ),
         );
     }
 }
