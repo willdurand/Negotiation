cbc866adfa2be99c7ab81715d5d3fa4713542abd
Neural Wetware <dlag368@gmail.com>
2015-06-08 21:48:26 +1000

    base implementation of match().

--- src/Negotiation/CharsetNegotiator.php
+++ src/Negotiation/CharsetNegotiator.php
@@ -2,9 +2,6 @@
 
 namespace Negotiation;
 
-/**
- * @author William Durand <william.durand1@gmail.com>
- */
 class CharsetNegotiator extends AbstractNegotiator
 {
 
@@ -16,20 +13,6 @@ class CharsetNegotiator extends AbstractNegotiator
     protected function typeFactory($type)
     {
         return new AcceptCharsetHeader($type);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function match(Header $charsetHeader, Header $priority, $index) {
-        $ac = $charsetHeader->getType();
-        $pc = $priority->getType();
-
-        if (!strcasecmp($ac, $pc) || $ac == '*') {
-            return new Match($charsetHeader->getQuality(), 0, $index);
-        }
-
-        return null;
     }
 
 }
