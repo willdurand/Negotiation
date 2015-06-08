8d6e76621730f67d03d0a7940e840f19dc6fd2cd
Neural Wetware <dlag368@gmail.com>
2015-06-08 21:14:15 +1000

    add test for reduce().

--- tests/Negotiation/Tests/AbstarctNegotiatorTest.php
+++ tests/Negotiation/Tests/AbstarctNegotiatorTest.php
@@ -28,4 +28,30 @@ class AbstractNegotiatorTest extends TestCase
         );
     }
 
+    /**
+     * @dataProvider dataProviderForTestReduce
+     */
+    public function testReduce($carry, $match, $expected)
+    {
+        $return = $this->call_private_method('\Negotiation\AbstractNegotiator', 'reduce', null, array($carry, $match));
+
+        $this->assertEquals($expected, $return);
+    }
+
+    public static function dataProviderForTestReduce()
+    {
+        return array(
+            array(
+                array(1 => new Match(1.0, 10, 1)),
+                new Match(0.5, 111, 1),
+                array(1 => new Match(0.5, 111, 1)),
+            ),
+            array(
+                array(0 => new Match(1.0, 10, 1)),
+                new Match(0.5, 111, 1),
+                array(0 => new Match(1.0, 10, 1), 1 => new Match(0.5, 111, 1)),
+            ),
+        );
+    }
+
 }
